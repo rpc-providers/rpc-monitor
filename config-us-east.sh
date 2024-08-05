07377a2a0b40b7404d5de953d068bbe1dadf08e3
rpc-providers <info@rpc-providers.net>
2024-08-05 22:29:04 +0200

    Updated providers

--- config-us-east.sh
+++ config-us-east.sh
@@ -3,25 +3,11 @@
 # Use an Associative Array for providers definition per zone
 declare -A rpcs
 
-rpcs[wss://polkadot-public-rpc.blockops.network/ws]=polkadot
-rpcs[wss://polkadot-rpc.dwellir.com]=polkadot
-#rpcs[wss://polkadot-rpc-tn.dwellir.com]=polkadot
 rpcs[wss://rpc.ibp.network/polkadot]=polkadot
-rpcs[wss://rpc.dotters.network/polkadot]=polkadot
-rpcs[wss://rpc-polkadot.luckyfriday.io]=polkadot
-rpcs[wss://polkadot.api.onfinality.io/public-ws]=polkadot
-rpcs[wss://polkadot.public.curie.radiumblock.co/ws]=polkadot
-rpcs[wss://rockx-dot.w3node.com/polka-public-dot/ws]=polkadot
-rpcs[wss://dot-rpc.stakeworld.io]=polkadot
-#rpcs[wss://kusama-public-rpc.blockops.network/ws]=kusama
-rpcs[wss://kusama-rpc.dwellir.com]=kusama
-#rpcs[wss://kusama-rpc-tn.dwellir.com]=kusama
 rpcs[wss://rpc.ibp.network/kusama]=kusama
+rpcs[wss://rpc.dotters.network/polkadot]=polkadot
 rpcs[wss://rpc.dotters.network/kusama]=kusama
 rpcs[wss://rpc-kusama.luckyfriday.io]=kusama
+rpcs[wss://rpc-polkadot.luckyfriday.io]=polkadot
 rpcs[wss://kusama.api.onfinality.io/public-ws]=kusama
-rpcs[wss://kusama.public.curie.radiumblock.co/ws]=kusama
-rpcs[wss://rockx-ksm.w3node.com/polka-public-ksm/ws]=kusama
-rpcs[wss://ksm-rpc.stakeworld.io]=kusama
-#rpcs[wss://kusama-rpc.publicnode.com]=kusama
-
+rpcs[wss://polkadot.api.onfinality.io/public-ws]=kusama
