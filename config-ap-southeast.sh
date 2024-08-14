52fa7c0ca1a17414ea2baf86f268f743fafee58c
rpc-providers <info@rpc-providers.net>
2024-08-14 17:18:24 +0200

    Clean up double entries

--- config-ap-southeast.sh
+++ config-ap-southeast.sh
@@ -3,9 +3,5 @@
 # Use an Associative Array for providers definition
 declare -A rpcs
 
-#rpcs[wss://rpc.ibp.network/kusama]=kusama
-#rpcs[wss://rpc.ibp.network/polkadot]=polkadot
 rpcs[wss://rockx-dot.w3node.com/polka-public-dot/ws]=polkadot
 rpcs[wss://rockx-ksm.w3node.com/polka-public-ksm/ws]=kusama
-rpcs[wss://rpc.dotters.network/polkadot]=polkadot
-rpcs[wss://rpc.dotters.network/kusama]=kusama
--- config-us-east.sh
+++ config-us-east.sh
@@ -3,10 +3,6 @@
 # Use an Associative Array for providers definition per zone
 declare -A rpcs
 
-rpcs[wss://rpc.ibp.network/polkadot]=polkadot
-rpcs[wss://rpc.ibp.network/kusama]=kusama
-rpcs[wss://rpc.dotters.network/polkadot]=polkadot
-rpcs[wss://rpc.dotters.network/kusama]=kusama
 rpcs[wss://rpc-kusama.luckyfriday.io]=kusama
 rpcs[wss://rpc-polkadot.luckyfriday.io]=polkadot
 rpcs[wss://kusama.api.onfinality.io/public-ws]=kusama
