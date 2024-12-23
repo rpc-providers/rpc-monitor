f673a90eeef3d89d571b015c27d2417c75e74c94
rpc-providers <info@rpc-providers.net>
2024-12-23 22:08:06 +0100

    Move to single config file

--- config-ap-southeast.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Use an Associative Array for providers definition
-declare -A rpcs
-
-rpcs[wss://rockx-dot.w3node.com/polka-public-dot/ws]=polkadot
-rpcs[wss://rockx-ksm.w3node.com/polka-public-ksm/ws]=kusama
-rpcs[wss://statemint.api.onfinality.io/public-ws]=polkadot-assethub
-rpcs[wss://bridgehub-polkadot.api.onfinality.io/public-ws]=polkadot-bridgehub
--- config-us-east.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Use an Associative Array for providers definition per zone
-declare -A rpcs
-
-rpcs[wss://rpc-kusama.luckyfriday.io]=kusama
-rpcs[wss://rpc-polkadot.luckyfriday.io]=polkadot
-rpcs[wss://kusama.api.onfinality.io/public-ws]=kusama
-rpcs[wss://polkadot.api.onfinality.io/public-ws]=polkadot
