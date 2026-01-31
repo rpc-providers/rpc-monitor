07b10e6a991824289ef8793d94786fdf6fc61256
rpc-providers <info@rpc-providers.net>
2026-01-31 20:53:07 +0100

    Config.sh update for tender 4

--- config.sh
+++ config.sh
@@ -3,9 +3,11 @@
 # Define all RPC providers in a single file with zone tagging and job label
 declare -A rpcs
 
-# Blockops
-rpcs[wss://polkadot-public-rpc.blockops.network/ws]="polkadot,eu-central,blockops"
+###############
+# DOT endpoints
+###############
 # Dwellir 
+rpcs[wss://polkadot-rpc.n.dwellir.com]="polkadot,eu-central,Dwellir"
 rpcs[wss://asset-hub-polkadot-rpc.n.dwellir.com]="polkadot-assethub,eu-central,Dwellir"
 rpcs[wss://bridge-hub-polkadot-rpc.n.dwellir.com]="polkadot-bridgehub,eu-central,Dwellir"
 rpcs[wss://collectives-polkadot-rpc.n.dwellir.com]="polkadot-collectives,eu-central,Dwellir"
@@ -16,41 +18,38 @@ rpcs[wss://rpc-polkadot.helixstreet.io]="polkadot,eu-central,Helixstreet"
 # Luckyfriday
 rpcs[wss://rpc-polkadot.luckyfriday.io]="polkadot,us-east,GlobalStake"
 rpcs[wss://rpc-asset-hub-polkadot.luckyfriday.io]="polkadot-assethub,us-east,GlobalStake"
-rpcs[wss://rpc-bridge-hub-polkadot.luckyfriday.io]="polkadot-bridgehub,us-east,GlobalStake"
 # Onfinality 
 rpcs[wss://polkadot.api.onfinality.io/public-ws]="polkadot,us-east,onfinality"
 rpcs[wss://statemint.api.onfinality.io/public-ws]="polkadot-assethub,us-east,onfinality"
-rpcs[wss://bridgehub-polkadot.api.onfinality.io/public-ws]="polkadot-bridgehub,us-east,onfinality"
-rpcs[wss://collectives.api.onfinality.io/public-ws]="polkadot-collectives,us-east,onfinality"
-rpcs[wss://coretime-polkadot.api.onfinality.io/public-ws]="polkadot-coretime,us-east,onfinality"
 # Radiumblock
 rpcs[wss://polkadot.public.curie.radiumblock.co/ws]="polkadot,us-east,RadiumBlock"
+rpcs[wss://statemint.public.curie.radiumblock.co/ws]="polkadot-assethub,us-east,RadiumBlock"
+rpcs[wss://bridgehub-polkadot.public.curie.radiumblock.co/ws]="polkadot-bridgehub,us-east,RadiumBlock"
+rpcs[wss://collectives.public.curie.radiumblock.co/ws]="polkadot-collectives,us-east,RadiumBlock"
+rpcs[wss://coretime-polkadot.public.curie.radiumblock.co/ws]="polkadot-coretime,us-east,RadiumBlock"
+rpcs[wss://people-polkadot.public.curie.radiumblock.co/ws]="polkadot-people,us-east,RadiumBlock"
 # SimplyStaking
 rpcs[wss://spectrum-03.simplystaking.xyz/cG9sa2Fkb3QtMDMtOTFkMmYwZGYtcG9sa2Fkb3Q/LjwBJpV3dIKyWQ/polkadot/mainnet/]="polkadot,us-east,Simply Staking"
 rpcs[wss://spectrum-03.simplystaking.xyz/cG9sa2Fkb3QtMDMtOTFkMmYwZGYtcG9sa2Fkb3Q/mgX--uWlEtmNKw/polkadotbridgehub/mainnet/]="polkadot-bridgehub,us-east,Simply Staking"
 # Stakeworld 
 rpcs[wss://dot-rpc.stakeworld.io]="polkadot,eu-central,stakeworld"
-rpcs[wss://dot-rpc.stakeworld.io/assethub]="polkadot-assethub,eu-central,stakeworld"
 ###############
 # KSM endpoints
 ###############
-# Blockops
-rpcs[wss://kusama-public-rpc.blockops.network/ws]="kusama,eu-central,blockops"
 # Dwellir
 rpcs[wss://kusama-rpc.n.dwellir.com]="kusama,eu-central,Dwellir"
-rpcs[wss://bridge-hub-kusama-rpc.n.dwellir.com]="kusama-bridgehub,eu-central,Dwellir"
 rpcs[wss://coretime-kusama-rpc.n.dwellir.com]="kusama-coretime,eu-central,Dwellir"
 rpcs[wss://encointer-kusama-rpc.n.dwellir.com]="kusama-encointer,eu-central,Dwellir"
-rpcs[wss://people-kusama-rpc.n.dwellir.com]="kusama-people,eu-central,Dwellir"
 # Helixstreet 
 rpcs[wss://rpc-kusama.helixstreet.io]="kusama,eu-central,Helixstreet"
 # Luckyfriday
-rpcs[wss://rpc-kusama.luckyfriday.io]="kusama,us-east,GlobalStake"
+rpcs[wss://rpc-asset-hub-kusama.luckyfriday.io]="kusama-assethub,us-east,GlobalStake"
 # Onfinality
-rpcs[wss://assethub-kusama.api.onfinality.io/public-ws]="kusama-assethub,us-east,onfinality"
-rpcs[wss://bridgehub-kusama.api.onfinality.io/public-ws]="kusama-bridgehub,us-east,onfinality"
+rpcs[wss://kusama.api.onfinality.io/public-ws]="kusama,us-east,onfinality"
 # Radiumblock
 rpcs[wss://statemine.public.curie.radiumblock.co/ws]="kusama-assethub,us-east,RadiumBlock"
+rpcs[wss://bridgehub-kusama.public.curie.radiumblock.co/ws]="kusama-bridgehub,us-east,RadiumBlock"
+rpcs[wss://people-kusama.public.curie.radiumblock.co/ws]="kusama-people,us-east,RadiumBlock"
 # SimplyStaking
 rpcs[wss://spectrum-03.simplystaking.xyz/cG9sa2Fkb3QtMDMtOTFkMmYwZGYtcG9sa2Fkb3Q/QXq7QZ6Q60NDzA/kusama/mainnet/]="kusama,us-east,Simply Staking"
 rpcs[wss://spectrum-03.simplystaking.xyz/cG9sa2Fkb3QtMDMtOTFkMmYwZGYtcG9sa2Fkb3Q/balkpUVauqyv8g/kusamabridgehub/mainnet/]="kusama-bridgehub,us-east,Simply Staking"
@@ -60,4 +59,4 @@ rpcs[wss://ksm-rpc.stakeworld.io]="kusama,eu-central,stakeworld"
 # WND endpoints
 ###############
 # Dwellir
-rpcs[wss://westend-rpc.n.dwellir.com]="westend,eu-central,Dwellir"
+rpcs[wss://westend.public.curie.radiumblock.co/ws]="westend,us-east,RadiumBlock"
