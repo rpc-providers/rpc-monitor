ed7a3092236f3691e77b85592af52ec1c4dcf122
rpc-providers <info@rpc-providers.net>
2026-06-01 19:59:16 +0200

    Remove radiumblock endpoints (discontinuation)

--- config.sh
+++ config.sh
@@ -21,13 +21,6 @@ rpcs[wss://rpc-asset-hub-polkadot.luckyfriday.io]="polkadot-assethub,us-east,Glo
 # Onfinality 
 rpcs[wss://polkadot.api.onfinality.io/public-ws]="polkadot,us-east,onfinality"
 rpcs[wss://statemint.api.onfinality.io/public-ws]="polkadot-assethub,us-east,onfinality"
-# Radiumblock
-rpcs[wss://polkadot.public.curie.radiumblock.co/ws]="polkadot,us-east,RadiumBlock"
-rpcs[wss://statemint.public.curie.radiumblock.co/ws]="polkadot-assethub,us-east,RadiumBlock"
-rpcs[wss://bridgehub-polkadot.public.curie.radiumblock.co/ws]="polkadot-bridgehub,us-east,RadiumBlock"
-rpcs[wss://collectives.public.curie.radiumblock.co/ws]="polkadot-collectives,us-east,RadiumBlock"
-rpcs[wss://coretime-polkadot.public.curie.radiumblock.co/ws]="polkadot-coretime,us-east,RadiumBlock"
-rpcs[wss://people-polkadot.public.curie.radiumblock.co/ws]="polkadot-people,us-east,RadiumBlock"
 # SimplyStaking
 rpcs[wss://spectrum-03.simplystaking.xyz/cG9sa2Fkb3QtMDMtOTFkMmYwZGYtcG9sa2Fkb3Q/LjwBJpV3dIKyWQ/polkadot/mainnet/]="polkadot,us-east,Simply Staking"
 rpcs[wss://spectrum-03.simplystaking.xyz/cG9sa2Fkb3QtMDMtOTFkMmYwZGYtcG9sa2Fkb3Q/mgX--uWlEtmNKw/polkadotbridgehub/mainnet/]="polkadot-bridgehub,us-east,Simply Staking"
@@ -46,10 +39,6 @@ rpcs[wss://rpc-kusama.helixstreet.io]="kusama,eu-central,Helixstreet"
 rpcs[wss://rpc-asset-hub-kusama.luckyfriday.io]="kusama-assethub,us-east,GlobalStake"
 # Onfinality
 rpcs[wss://kusama.api.onfinality.io/public-ws]="kusama,us-east,onfinality"
-# Radiumblock
-rpcs[wss://statemine.public.curie.radiumblock.co/ws]="kusama-assethub,us-east,RadiumBlock"
-rpcs[wss://bridgehub-kusama.public.curie.radiumblock.co/ws]="kusama-bridgehub,us-east,RadiumBlock"
-rpcs[wss://people-kusama.public.curie.radiumblock.co/ws]="kusama-people,us-east,RadiumBlock"
 # SimplyStaking
 rpcs[wss://spectrum-03.simplystaking.xyz/cG9sa2Fkb3QtMDMtOTFkMmYwZGYtcG9sa2Fkb3Q/QXq7QZ6Q60NDzA/kusama/mainnet/]="kusama,us-east,Simply Staking"
 rpcs[wss://spectrum-03.simplystaking.xyz/cG9sa2Fkb3QtMDMtOTFkMmYwZGYtcG9sa2Fkb3Q/balkpUVauqyv8g/kusamabridgehub/mainnet/]="kusama-bridgehub,us-east,Simply Staking"
@@ -58,5 +47,3 @@ rpcs[wss://ksm-rpc.stakeworld.io]="kusama,eu-central,stakeworld"
 ###############
 # WND endpoints
 ###############
-# Dwellir
-rpcs[wss://westend.public.curie.radiumblock.co/ws]="westend,us-east,RadiumBlock"
